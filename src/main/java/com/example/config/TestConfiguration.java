a6f1e652c86c745e1c0e06e633629569e0593693
viniD <dangwal.vinita@gmail.com>
2024-05-24 11:00:08 -0400

    minor code changes

--- src/main/java/com/example/config/TestConfiguration.java
+++ src/main/java/com/example/config/TestConfiguration.java
@@ -5,7 +5,7 @@ public class TestConfiguration {
         private TestConfiguration(){
 
         }
-       // public static final String BASE_URL = "http://localhost:4200";
+       // public static final String BASE_URL = "http://localhost:8080";
         public static final String BASE_URL = "https://vinidan.pythonanywhere.com";
 
 
