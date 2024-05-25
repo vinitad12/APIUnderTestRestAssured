7e5952d0b30c28d5a5e8acbfce1f268129e9a1f9
viniD <dangwal.vinita@gmail.com>
2024-05-25 17:06:25 -0400

    utils updated to get datetime for reporting and updated report

--- src/main/java/com/example/config/TestConfiguration.java
+++ src/main/java/com/example/config/TestConfiguration.java
@@ -4,7 +4,7 @@ public class TestConfiguration {
 
     // update url to use localhost if using localhost setup with port 8080
     //public static final String DEFAULT_BASE_URL = "https://vinidan.pythonanywhere.com";
-    //public static final String DEFAULT_BASE_URL = "http://localhost:4200";
+    public static final String DEFAULT_BASE_URL = "http://localhost:4200";
 
 
 }
